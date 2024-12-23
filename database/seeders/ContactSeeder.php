a3841093882a86018fdffb68f882850a418cddad
monn <pphan@paragoniu.edu.kh>
2024-12-24 02:37:43 +0700

    newly

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Review;
+
+class ReviewSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Review::factory(100)->create(); // Generate 100 reviews
+    }
+}
---
+++ database/seeders/TicketSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Ticket;
+
+class TicketSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Ticket::factory(30)->create(); // Generate 30 tickets
+    }
+}
